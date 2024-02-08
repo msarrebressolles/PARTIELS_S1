a428590ed58a20fe62d9fc4d39d4151c1bee5b08
msarrebressolles <maelsrbs@gmail.com>
2024-02-08 09:19:26 +0000

    premier commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+ const float coutHoraire = 4.0;
+ int velosDispos = 20;
+ int capacite= 20;
+ int choix;
+ cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+ cout << "-----------------------------------------" << endl;
+ //Partie à compléter 3.1
+ cout << "-----------------------------------------" << endl<< endl;
+ cout << "1. Emprunter un vélô électrique" << endl;
+ cout << "2. Restituer un vélô électrique" << endl;
+ cout << "3. Estimer le coût de la location" << endl;
+ cout << "4. Quitter l'application" << endl << endl;
+ return 0;
+}
